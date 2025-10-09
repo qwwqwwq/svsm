2222569d38b9de20f8f0c0a6494450090cdf483f
Ubuntu <ubuntu@c3-large-x86-nyc-1.maas>
2025-10-09 15:05:51 +0000

    comment out #[cfg(feature = vtpm_H8_ek)]

--- kernel/src/vtpm/tcgtpm/ek_templates.rs
+++ kernel/src/vtpm/tcgtpm/ek_templates.rs
@@ -261,7 +261,7 @@
 // ];
 
 // Template H-8: RSA-SSA 2048 (Signing)
-#[cfg(feature = "vtpm_H8_ek")]
+//#[cfg(feature = "vtpm_H8_ek")]
 pub const DEFAULT_PUBLIC_AREA: [u8; 312] = [
     0x00, 0x01, // type TPM_ALG_RSA
     0x00, 0x0B, // nameAlg TPM_ALG_SHA256
